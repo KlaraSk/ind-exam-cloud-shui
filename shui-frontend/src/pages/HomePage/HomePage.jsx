f924258ddaec3299f9d155d9b2e6ed504359483f
KlaraSkStudent <3kdyhfe24.skolkl@folkuniversitetet.nu>
2025-09-29 08:51:58 +0200

    Följande API-anrop är testade: POST login, POST register user, GET messages, POST message

--- shui-frontend/src/pages/HomePage/HomePage.jsx
+++ shui-frontend/src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,44 @@
+import { useEffect, useState } from "react";
+import { getMessages } from "../../api/messages";
 import "./HomePage.css";
+import { loginApi, registerApi } from "../../api/auth";
 
 function HomePage() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const setupMessages = async () => {
+      const response = await getMessages();
+      setMessages(response.data);
+    };
+    setupMessages();
+    loginUser();
+    // regTest();
+  }, []);
+
+  useEffect(() => {
+    console.log("messages: ", messages);
+  }, [messages]);
+
+  const loginUser = async (e) => {
+    // e.preventDefault();
+    const result = await loginApi({
+      username: "klara",
+      password: "Test123!",
+    });
+    console.log(result.data.message);
+  };
+
+  const regTest = async (data) => {
+    const result = await registerApi({
+      username: data.username,
+      password: data.password,
+      email: data.email,
+      role: data.role,
+    });
+    console.log(result.data);
+  };
+
   return <section className="page">HomePage</section>;
 }
 
